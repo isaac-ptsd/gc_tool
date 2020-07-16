bd2a7020ca8fda7560aec0aa646ec68a4d2ffdf3
Isaac Stoutenburgh <isaac.stoutenburgh@phoenix.k12.or.us>
2020-07-16 16:59:28 -0700

    pulling assignment names

--- pull_grades.py
+++ pull_grades.py
@@ -40,15 +40,20 @@ if not creds or not creds.valid:
 
 service = build('classroom', 'v1', credentials=creds)
 
+
 ###############################################
 # Credentials / Authorization
 ###############################################
+def assignment_lookup(course_id_in, id_in):
+    # api call
+    course_info = service.courses().courseWork().get(courseId=course_id_in, id=id_in).execute()
+    return course_info["title"]
 
 
 def student_lookup(user_id):
     """
     :param user_id:
-    :return: users name: (familyName,  givenName)
+    :return: users name: "familyName,  givenName"
     """
     user_info = service.userProfiles().get(userId=user_id).execute()
     return user_info["name"]["familyName"] + ", " + user_info["name"]["givenName"]
@@ -90,9 +95,14 @@ def to_csv(list_of_dicts_in, name_of_csv_to_create):
         print(e)
 
 
-@Gooey(program_name="Fetch Grades",)
+@Gooey(program_name="Fetch Grades", )
 def main():
     # API call to get classes
+    """TODO: 1) look up students by name in template and update when found
+                Goal is to preserve student ID's
+             2) Pull all grades for all assignments by class. Create a separate import template for each assignment.
+    """
+
     results = service.courses().list(pageSize=10).execute()
 
     # create list of course names to select from
@@ -127,6 +137,19 @@ def main():
     # API call to get course work
     student_submissions = service.courses().courseWork().studentSubmissions().list(courseId=slctd_crs_id,
                                                                                    courseWorkId='-').execute()
+    print("course API call return value: ", student_submissions)
+
+    print("CourseWorkId's: ")
+    assignment_dict = [{c['courseId']: c['courseWorkId']} for c in student_submissions['studentSubmissions']]
+    print(assignment_dict)
+    for ass_dic in assignment_dict:
+        print("ass_dic: --------------->", ass_dic)
+        for k, v in ass_dic.items():
+            print("ASSIGNMENT LOOKUP ------------> ", assignment_lookup(k, v))
+
+    # print("ASSIGNMENT LOOKUP ------------> ", assignment_lookup("115992425191", "116601796515"))
+
+    name_grade_dict_list = []
     try:
         id_grade = get_userId_grade(student_submissions)
         print("get_grades: ", id_grade)
@@ -134,14 +157,31 @@ def main():
         for d in get_userId_grade(student_submissions):
             for key in d:
                 print(" ", student_lookup(key))
+        name_grade_dict_list = swap_id_for_name(id_grade)
         print("swap_id_for_name: ", swap_id_for_name(id_grade))
     except KeyError:
         print("No grades found for ", selected_course)
     except:
         print("uh-oh something broke")
 
+    grade_template = user_inputs['grade_template']
+    save_path = user_inputs['output_directory']
+    df = pd.read_csv(grade_template, header=None)
+    df = df.drop(df.index[7:100])
+    df.columns = ['A', 'B', 'C']
+    start_row_index = 7
+    df.at[1, 'B'] = selected_course
+    print(df)
+    for d in name_grade_dict_list:
+        print("PRINTING D: ", d)
+        for key, value in d.items():
+            df.at[start_row_index, 'B'] = key
+            df.at[start_row_index, 'C'] = value
+            start_row_index += 1
+
+    output_file = os.path.join(save_path, selected_course.replace(" ", "_") + "_grade_template.csv")
+    df.to_csv(output_file, header=False, index=False)
+
 
 if __name__ == '__main__':
     main()
-
-
